965234db340ab5e030e9dbae1b8d06acb98dbc9f
wiktor <wkilijan@gmail.com>
2020-05-31 19:18:53 +0200

    aspect

--- src/main/java/udemy/spring/todoapp/controller/TaskGroupController.java
+++ src/main/java/udemy/spring/todoapp/controller/TaskGroupController.java
@@ -22,6 +22,7 @@ import java.net.URI;
 import java.util.List;
 
 @Controller
+@IllegalExceptionProcessng
 @RequestMapping("/groups")
 public class TaskGroupController {
 
@@ -60,18 +61,6 @@ public class TaskGroupController {
     ResponseEntity<?> toggleGroup(@PathVariable int id) {
         taskGroupService.toggleGroup(id);
         return ResponseEntity.noContent().build();
-    }
-
-    @ResponseBody
-    @ExceptionHandler(IllegalArgumentException.class)
-    ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
-        return ResponseEntity.notFound().build();
-    }
-
-    @ResponseBody
-    @ExceptionHandler(IllegalStateException.class)
-    ResponseEntity<String> handleIllegalState(IllegalStateException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
     // ============ FOR FRONTEND ============
